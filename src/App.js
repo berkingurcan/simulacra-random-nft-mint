004138e3b1e6d1b288d29a9539c86b36955f2e59
berkingurcan <berkingurcan@yandex.com>
2022-01-25 10:21:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+import Images from './Images'
+import Button from '@mui/material/Button'
+import {contractAddress, contractAbi} from "./contract.js"
+import { ethers} from "ethers"
+import {LinearProgress} from "@mui/material"
+import { useContractFunction } from "@usedapp/core"
+import BigNumber from 'bignumber.js'
 
 function App() {
+//CONNECT METAMASK
+  const [accounts, setAccounts] = useState([])
+  async function connectAccounts() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts"
+      })
+      setAccounts(accounts)
+    }
+  }
+
+  useEffect(() => {
+    connectAccounts()
+  }, [])
+
+  const [selectedImg, setSelectedImg] = useState()
+
+  async function showSims() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const address = await signer.getAddress( )
+    const contract = new ethers.Contract(
+      contractAddress,
+      contractAbi,
+      signer
+    )
+    const latestSim = contract.seeLatestSim()
+    console.log('work')
+    console.log(BigNumber.from(latestSim))
+    return(Images[latestSim-1])
+  }
+
+  //MINTING
+  async function handleMint() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractAbi,
+        signer
+      )
+      try {
+        const response = await contract.buySim()
+        console.log(response)
+
+      } catch(err) {
+        console.log(err)
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className='imgContainer'>
+          {Images.map((img, index) => (
+            <img key={index} src={img} alt="sim" />
+          ))}
+        </div>
+        {accounts.length && (<div><Button variant="contained" onClick={handleMint}>Mint a Random SIM</Button> </div>)}
+{/*         <Button variant='outlined' onClick={showSims}>See Your Sim</Button>
+        <img src={ selectedImg } alt="minted" className='minted' /> */}
+
+        <p>Add Your Wallet SIM NFT 0x23e2b96895ccAE234cA87965bfB4f7B9eE62f914s</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Images.js
@@ -0,0 +1,10 @@
+export default [
+   "https://gateway.pinata.cloud/ipfs/QmWWL89GWsLN6eyCVc9ZJaUaNS2KAd2R5jhmphtzQgSdNM/1.gif",
+   "https://gateway.pinata.cloud/ipfs/QmWWL89GWsLN6eyCVc9ZJaUaNS2KAd2R5jhmphtzQgSdNM/2.gif",
+   "https://gateway.pinata.cloud/ipfs/QmWWL89GWsLN6eyCVc9ZJaUaNS2KAd2R5jhmphtzQgSdNM/3.gif",
+   "https://gateway.pinata.cloud/ipfs/QmWWL89GWsLN6eyCVc9ZJaUaNS2KAd2R5jhmphtzQgSdNM/4.gif",
+   "https://gateway.pinata.cloud/ipfs/QmWWL89GWsLN6eyCVc9ZJaUaNS2KAd2R5jhmphtzQgSdNM/5.gif",
+   "https://gateway.pinata.cloud/ipfs/QmWWL89GWsLN6eyCVc9ZJaUaNS2KAd2R5jhmphtzQgSdNM/6.gif",
+   "https://gateway.pinata.cloud/ipfs/QmWWL89GWsLN6eyCVc9ZJaUaNS2KAd2R5jhmphtzQgSdNM/7.gif",
+   "https://gateway.pinata.cloud/ipfs/QmWWL89GWsLN6eyCVc9ZJaUaNS2KAd2R5jhmphtzQgSdNM/8.gif"
+];
